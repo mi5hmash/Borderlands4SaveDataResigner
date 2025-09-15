26d6535134936bba49cece35c73bce0d8ae8de28
Michał Gębicki <59212335+mi5hmash@users.noreply.github.com>
2025-09-15 17:56:10 +0200

    Initial commit

---
+++ Borderlands4SaveDataResigner/Helpers/UrlHelpers.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace Borderlands4SaveDataResigner.Helpers;
+
+public static class UrlHelpers
+{
+    /// <summary>
+    /// Opens the given URL in the default web browser.
+    /// </summary>
+    /// <param name="url"></param>
+    /// <exception cref="PlatformNotSupportedException"></exception>
+    public static void OpenUrl(this string url)
+    {
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            Process.Start(new ProcessStartInfo(url) { UseShellExecute = true });
+        else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux) || RuntimeInformation.IsOSPlatform(OSPlatform.FreeBSD))
+            Process.Start("xdg-open", url);
+        else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            Process.Start("open", url);
+        else
+            throw new PlatformNotSupportedException("Unsupported OS platform.");
+    }
+}
---
+++ Borderlands4SaveDataResigner/Types/EpicId.cs
@@ -0,0 +1,72 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Text.RegularExpressions;
+using Borderlands4SaveDataResigner.Helpers;
+
+namespace Borderlands4SaveDataResigner.Types;
+
+/// <summary>
+/// A class representing an Epic ID.
+/// </summary>
+public class EpicId
+{
+    private const int Length = 32;
+    private readonly string _pattern = $"^[0-9a-fA-F]{{{Length}}}$";
+
+    /// <summary>
+    /// The account ID of the user.
+    /// </summary>
+    public string AccountId { get; private set; } = "00000000000000000000000000000000";
+
+    /// <summary>
+    /// Attempts to set the Epic ID if it matches the required pattern.
+    /// </summary>
+    /// <param name="epicId">The Epic ID to validate and set. Must match the required pattern.</param>
+    /// <returns><see langword="true"/> if the Epic ID is valid and successfully set; otherwise, <see langword="false"/>.</returns>
+    public bool TrySetEpicId(string epicId)
+    {
+        epicId = epicId.Trim();
+        if (!Regex.IsMatch(epicId, _pattern)) return false;
+        AccountId = epicId;
+        return true;
+    }
+
+    /// <summary>
+    /// Converts the <see cref="AccountId"/> string to a byte array representation.
+    /// </summary>
+    /// <returns>A byte array representing the <see cref="AccountId"/> string.</returns>
+    public byte[] GetAsByteArray()
+    {
+        var bytes = new byte[AccountId.Length];
+        for (var i = 0; i < AccountId.Length; i++)
+            bytes[i] = (byte)AccountId[i];
+        return bytes;
+    }
+
+    /// <summary>
+    /// Opens the EGS profile URL in the default web browser.
+    /// </summary>
+    public void OpenEpicProfileUrl()
+    {
+        var url = $"https://store.epicgames.com/u/{AccountId}";
+        try { url.OpenUrl(); }
+        catch
+        {
+            // ignored
+        }
+    }
+
+    public bool Equals(EpicId other)
+        => AccountId == other.AccountId;
+    
+    public override bool Equals([NotNullWhen(true)] object? obj)
+        => obj is EpicId castedObj && Equals(castedObj);
+
+    public override int GetHashCode()
+        => HashCode.Combine(AccountId);
+
+    public static bool operator ==(EpicId left, EpicId right)
+        => left.Equals(right);
+
+    public static bool operator !=(EpicId left, EpicId right)
+        => !(left == right);
+}
